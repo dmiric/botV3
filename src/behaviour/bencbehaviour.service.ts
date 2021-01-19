3324236d302d3822030d36745f67db2d14027d66
dmiric <damir.miric@gmail.com>
2021-01-19 22:29:45 +0100

    t

--- src/behaviour/bencbehaviour.service.ts
+++ src/behaviour/bencbehaviour.service.ts
@@ -118,6 +118,7 @@ export class BencBehaviourService {
             }
             candleStack.push(candle)
         }
+        console.log(candleStack)
         return candleStack.reverse()
     }
 
